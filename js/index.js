97f74fd19c2774988c6e7117cbf3f1287d5a74b6
Jidan Das <68393702+Itshak04@users.noreply.github.com>
2024-07-16 14:14:55 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function openForm(el) {
+    var popIn = document.getElementById("createForm");
+    popIn.style.display = "block";
+  }
+  
+  function closeForm() {
+    var popOut = document.getElementById("createForm");
+    popOut.style.display = "none";
+  }
+  
